0af02b7fb1460ac67370f6f36c96bc3f5f5d5624
you13_lib <you13lib@gmail.com>
2024-02-24 01:00:33 +0900

    第17章

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,6 +10,11 @@ import {
   CardsSkeleton,
   CardSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   const {
